f27954f8625638946d3d3c85afaa1ad1a09dea3f
Victor Bianchi <vi.bianchi10@gmail.com>
2023-07-30 21:52:01 -0300

    Implementação do CRUD: GET e POST

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
     define('HOST', 'localhost');
-    define('DATABASE', 'pe1');
+    define('DATABASE', 'api_project');
     define('USERNAME', 'root');
     define('PASSWORD', '');
 ?>
--- public/index.php
+++ public/index.php
@@ -15,10 +15,36 @@
     require_once '../vendor/autoload.php';
 
     $url = isset($_GET['url']) ? explode('/', $_GET['url']) : '';
+    $method = $_SERVER['REQUEST_METHOD'];
     
     if(is_array($url)) {
         if($url[0] == 'produtos') {
-            echo "Produtos!";
+            switch ($method) {
+                case 'GET':
+                    $sql = MySQL::connect()->prepare('SELECT * FROM `produtos`');
+                    $sql->execute();
+                    $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+                    echo json_encode($data);
+                    break;
+                case 'POST':
+                    $nome = $_POST['name'];
+                    $marca = $_POST['brand'];
+                    $preco = $_POST['price'];
+                    $sql = MySQL::connect()->prepare('INSERT INTO `produtos` (`nome`, `marca`, `valor`) VALUES (?, ?, ?)');
+                    $sql->execute([$nome, $marca, $preco]);
+                    echo json_encode(['response'=>'ok']);
+                    break;
+                case 'PUT':
+                    # alterar dados no db
+                    echo "PUT";
+                    break;
+                case 'DELETE':
+                    # excluir dados do db
+                    echo "DEL";
+                    break;
+                default:
+                    die('ERRO: operação não suportada, cheque a documentação');
+            }
         } else {
             die('Operação de API inválida, cheque a documentação');
         }
