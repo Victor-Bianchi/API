43300b6fac1cf604db1b2721fe2de9627cb3025b
Victor Bianchi <vi.bianchi10@gmail.com>
2023-07-30 21:04:45 -0300

    Adição de funcionalidades do MySQL e estruturação básica de rotas

--- app/MySQL.php
+++ app/MySQL.php
@@ -9,5 +9,30 @@
             $pdo = new \PDO('mysql:host='.\HOST.';dbname='.\DATABASE.';', \USERNAME, \PASSWORD);
             return $pdo;
         }
+
+        public static function execQuery($query, $showData = false)
+        {
+            switch ($showData) {
+                case false:
+                    $sql = self::connect()->prepare($query);
+                    return $sql->execute();
+                    break;
+                case true:
+                    $sql = self::connect()->prepare($query);
+                    $sql->execute();
+                    $data = $sql->fetchAll(\PDO::FETCH_ASSOC);
+                    echo "<pre>";
+                    var_dump($data);
+                    return;
+                    break;
+                default:
+                    die('ERRO: Parâmetro inválido: '.$showData);
+            }
+        }
+
+        public static function execQueryQuickly($query)
+        {
+            return self::connect()->exec($query);
+        }
     }
 ?>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,28 @@
 <?php
+    
+    ###########
+    ##  API  ##
+    ###########
+
+    #   Objetivo: criar uma API aberta que forneça funções de CRUD onde o usuário tem opções de:
+    # - Ver todos os produtos cadastrados usando o GET
+    # - Adicionar novos produtos via POST
+    # - Remover um item da lista via DEL
+    # - Alterar dados do item da lista via PUT
+
+    use App\MySQL;
 
     require_once '../vendor/autoload.php';
 
+    $url = isset($_GET['url']) ? explode('/', $_GET['url']) : '';
     
+    if(is_array($url)) {
+        if($url[0] == 'produtos') {
+            echo "Produtos!";
+        } else {
+            die('Operação de API inválida, cheque a documentação');
+        }
+    } else {
+        die();
+    }
 ?>
